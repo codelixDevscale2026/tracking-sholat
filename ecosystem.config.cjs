f8139463fa3ac4215f3c8fe77e99449edb7e3384
anasMuf <anas.muhammadakbar@gmail.com>
2026-03-26 10:25:31 +0700

    style: Adjust indentation from spaces to tabs in ecosystem.config.cjs.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,20 +1,20 @@
 module.exports = {
-  apps: [
-    {
-      name: "tracking-sholat-api",
-      script: "pnpm",
-      args: "run --filter=api start",
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-    {
-      name: "tracking-sholat-platform",
-      script: "pnpm",
-      args: "run --filter=platform preview -- --port 3000 --host 0.0.0.0",
-      env: {
-        NODE_ENV: "production",
-      },
-    }
-  ]
+	apps: [
+		{
+			name: "tracking-sholat-api",
+			script: "pnpm",
+			args: "run --filter=api start",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+		{
+			name: "tracking-sholat-platform",
+			script: "pnpm",
+			args: "run --filter=platform preview -- --port 3000 --host 0.0.0.0",
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+	],
 };
